6a2427d9ff477090929232546fef6152504ef7ae
Manpreet Singh <msp99000@gmail.com>
2023-11-14 21:54:22 +0530

    chore: reroutes the pages URLs to updated ones

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../assets/images/favicon.png" rel="icon">
@@ -8,24 +9,24 @@
     <link rel="stylesheet" href="../css/cards.css">
     <script src="../js/script.js"></script>
 </head>
+
 <body>
     <?php include "topbar.php"; ?>
-    <?php include "process.php"; ?> 
-    
+    <?php include "process.php"; ?>
+
     <div class="welcome-text">
-        <img style="color:blue; max-width: 60px;" src="../assets/images/adminblue.svg" >
-        <div> 
+        <img style="color:blue; max-width: 60px;" src="../assets/images/adminblue.svg">
+        <div>
             Welcome <?php echo $_SESSION['firstName']; ?>
         </div>
     </div>
-    
+
     <div class="card-container">
 
         <a href="viewAdmins.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/admin.png" >
-                    <!-- <h4 class="card-title"><?php echo $adminCount ?></h4> -->
+                    <img src="../assets/images/admin.png">
                 </div>
                 <div class="card-body">
                     <p id="totalAttendance" class="card-value">Admins</p>
@@ -36,8 +37,7 @@
         <a href="viewAttendance.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/attendance.png" >
-                    <!-- <h4 class="card-title"><?php echo $attendanceCount ?></h4> -->
+                    <img src="../assets/images/attendance.png">
                 </div>
                 <div class="card-body">
                     <p id="totalAttendance" class="card-value">Attendance</p>
@@ -48,8 +48,7 @@
         <a href="viewInstructors.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/teacher.png" >
-                    <!-- <h4 class="card-title"><?php echo $instructorCount ?></h4> -->
+                    <img src="../assets/images/teacher.png">
                 </div>
                 <div class="card-body">
                     <p id="totalAttendance" class="card-value">Instructors</p>
@@ -60,8 +59,7 @@
         <a href="viewStudents.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/student.png" >
-                    <!-- <h4 class="card-title"><?php echo $studentCount ?></h4> -->
+                    <img src="../assets/images/student.png">
                 </div>
                 <div class="card-body">
                     <p id="totalStudents" class="card-value">Students</p>
@@ -72,39 +70,37 @@
         <a href="viewClasses.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/classes.png" >
-                    <!-- <h4 class="card-title"><?php echo $classCount ?></h4> -->
+                    <img src="../assets/images/classes.png">
                 </div>
                 <div class="card-body">
                     <p id="totalClasses" class="card-value">Classes</p>
                 </div>
-            </div> 
+            </div>
         </a>
 
         <a href="viewCourses.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/courses.png" >
-                    <!-- <h4 class="card-title"><?php echo $courseCount ?></h4> -->
+                    <img src="../assets/images/courses.png">
                 </div>
                 <div class="card-body">
                     <p id="totalClasses" class="card-value">Courses</p>
                 </div>
-            </div> 
+            </div>
         </a>
 
         <a href="viewSessions.php">
             <div class="card">
                 <div class="card-header">
-                    <img src="../assets/images/sessions.png" >
-                    <!-- <h4 class="card-title"><?php echo $sessionCount ?></h4> -->
+                    <img src="../assets/images/sessions.png">
                 </div>
                 <div class="card-body">
                     <p id="totalTerms" class="card-value">Sessions</p>
                 </div>
-            </div> 
+            </div>
         </a>
     </div>
     <?php include "../components/footer.php"; ?>
 </body>
+
 </html>
