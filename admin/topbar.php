2476e98e2fced53b9535ff1a230ca6e19dc30af7
Manpreet Singh <msp99000@gmail.com>
2023-11-15 23:47:19 +0530

    feat: adds attendance taking and viewing options

--- admin/topbar.php
+++ admin/topbar.php
@@ -10,36 +10,36 @@
       <div class="bar"></div>
     </div>
     <ul class="nav-list">
+      <li class="expander" id="admins-expander">
+        <a class="nav-list" href="#">Attendance</a>
+        <ul class="sub-list" id="admins-submenu">
+          <li><a href="viewAdmins.php">Instructor Attendance</a></li>
+          <li><a href="addAdmin.php">Mark Attendance</a></li>
+        </ul>
+      </li>
       <li class="expander" id="instructors-expander">
         <a class="nav-list" href="#">Instructors</a>
         <ul class="sub-list" id="instructors-submenu">
-          <li><a href="../pages/viewInstructors.php">View Instructors</a></li>
-          <li><a href="../pages/addInstructor.php">Add Instructor</a></li>
-          <li><a href="../pages/removeInstructor.php">Remove Instructor</a></li>
-          <li><a href="../pages/updateInstructor.php">Update Instructor</a></li>
+          <li><a href="viewInstructors.php">View Instructors</a></li>
+          <li><a href="addInstructor.php">Add Instructor</a></li>
+          <li><a href="removeInstructor.php">Remove Instructor</a></li>
+          <li><a href="updateInstructor.php">Update Instructor</a></li>
         </ul>
       </li>
       <li class="expander" id="students-expander">
         <a class="nav-list" href="#">Students</a>
         <ul class="sub-list" id="students-submenu">
-          <li><a href="../pages/viewStudents.php">View Students</a></li>
-          <li><a href="../pages/addStudent.php">Add Student</a></li>
-          <li><a href="../pages/removeStudent.php">Remove Student</a></li>
-          <li><a href="../pages/updateStudent.php">Update Student</a></li>
+          <li><a href="viewStudents.php">View Students</a></li>
+          <li><a href="addStudent.php">Add Student</a></li>
+          <li><a href="removeStudent.php">Remove Student</a></li>
+          <li><a href="updateStudent.php">Update Student</a></li>
         </ul>
       </li>
       <li class="expander" id="sessions-expander">
         <a class="nav-list" href="#">Sessions</a>
         <ul class="sub-list" id="sessions-submenu">
-          <li><a href="../pages/viewSessions.php">View Sessions</a></li>
-          <li><a href="../pages/addSession.php">Add New Session</a></li>
-        </ul>
-      </li>
-      <li class="expander" id="admins-expander">
-        <a class="nav-list" href="#">Attendance</a>
-        <ul class="sub-list" id="admins-submenu">
-          <li><a href="../pages/viewAdmins.php">Instructor Attendance</a></li>
-          <li><a href="../pages/addAdmin.php">Mark Attendance</a></li>
+          <li><a href="viewSessions.php">View Sessions</a></li>
+          <li><a href="addSession.php">Add New Session</a></li>
         </ul>
       </li>
       <li class="expander" id="students-expander">
@@ -48,7 +48,8 @@
         </a>
         <ul class="sub-list" id="students-submenu">
           <li style="width: auto;"><a href="../scripts/logout.php">Logout</a></li>
-          <li style="width: auto;"><a href="../pages/updateAdmin.php">Edit Profile</a></li>
+          <li style="width: auto;"><a href="updateAdmin.php">Update Profile</a></li>
+          <li style="width: auto;"><a href="viewAdmins.php">View Profile</a></li>
         </ul>
       </li>
     </ul>
