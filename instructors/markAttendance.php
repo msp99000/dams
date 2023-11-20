10b84f935d31b7bbd4d8671c1ec9f8807572cd24
Manpreet Singh <msp99000@gmail.com>
2023-11-21 03:33:41 +0530

    feat: adds feature to take attendance

--- instructors/markAttendance.php
+++ instructors/markAttendance.php
@@ -1,101 +1,124 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php include "heads.php"; ?>
+  <?php include "heads.php"; ?>
 </head>
+
 <body>
-    <?php include "topbar.php"; ?>
+  <?php include "topbar.php"; ?>
+  <?php include "process.php"; ?>
 
-<div class="container-fluid" id="container-wrapper">
-          <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Take Attendance (Today's Date : <?php echo $todaysDate = date("m-d-Y");?>)</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="./">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Instructor Panel</li>
-            </ol>
-          </div>
+  <div class="container-fluid" id="container-wrapper">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Take Attendance (Today's Date : <?php echo date("m-d-Y"); ?>)</h1>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="./">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Instructor Panel</li>
+      </ol>
+    </div>
 
-          <div class="row">
-            <div class="col-lg-12">
-
+    <div class="row">
+      <div class="col-lg-12">
 
         <form method="post">
-            <div class="row">
-              <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-12">
               <div class="card mb-4">
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">All Student in (<?php echo $rrw['className'].' - '.$rrw['classArmName'];?>) Class</h6>
+                  <h6 class="m-0 font-weight-bold text-primary">All Student in (<?php echo $rrw['className'] . ' - ' . $rrw['classArmName']; ?>) Class</h6>
                   <h6 class="m-0 font-weight-bold text-danger">Note: <i>Click on the checkboxes besides each student to take attendance!</i></h6>
                 </div>
                 <div class="table-responsive p-3">
-                <?php echo $statusMsg; ?>
+                  <?php echo $statusMsg; ?>
                   <table class="table align-items-center table-flush table-hover">
-                        <thead class="thead-light">
-                            <tr>
-                                <th>Attendance ID</th>
-                                <th>Course ID</th>
-                                <th>Instructor ID</th>
-                                <th>Student ID</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                                <th>Check</th>
-                            </tr>
-                        </thead>
-                    
+                    <thead class="thead-light">
+                      <tr>
+                        <th>S. no.</th>
+                        <th>Student ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Course ID</th>
+                        <th>Class ID</th>
+                        <th>Check</th>
+                      </tr>
+                    </thead>
+
                     <tbody>
 
-                  <?php
-                      $query = "SELECT tblstudents.Id,tblstudents.admissionNumber,tblclass.className,tblclass.Id As classId,tblclassarms.classArmName,tblclassarms.Id AS classArmId,tblstudents.firstName,
-                      tblstudents.lastName,tblstudents.otherName,tblstudents.admissionNumber,tblstudents.dateCreated
-                      FROM tblstudents
-                      INNER JOIN tblclass ON tblclass.Id = tblstudents.classId
-                      INNER JOIN tblclassarms ON tblclassarms.Id = tblstudents.classArmId
-                      where tblstudents.classId = '$_SESSION[classId]' and tblstudents.classArmId = '$_SESSION[classArmId]'";
-
-                      $query = "SELECT students.student_id, ";
-
+                      <?php
+                      $query = "SELECT s.student_id, s.first_name, s.last_name, course_id, class_id 
+                                FROM students s  
+                                INNER JOIN schedules sc USING (student_id) 
+                                INNER JOIN classes cl USING (class_id) 
+                                INNER JOIN courses c USING (course_id) 
+                                WHERE instructor_id = '" . $_SESSION['instructor_id'] . "'";
 
                       $rs = $conn->query($query);
-                      $num = $rs->num_rows;
-                      $sn=0;
-                      $status="";
-                      if($num > 0)
-                      { 
-                        while ($rows = $rs->fetch_assoc())
-                          {
-                             $sn = $sn + 1;
-                            echo"
+                      $sn = 0;
+                      $status = "";
+                      if ($rs) {
+                        while ($rows = $rs->fetch_assoc()) {
+                          $sn++;
+                          echo "
                               <tr>
-                                <td>".$sn."</td>
-                                <td>".$rows['firstName']."</td>
-                                <td>".$rows['lastName']."</td>
-                                <td>".$rows['otherName']."</td>
-                                <td>".$rows['admissionNumber']."</td>
-                                <td>".$rows['className']."</td>
-                                <td>".$rows['classArmName']."</td>
-                                <td><input name='check[]' type='checkbox' value=".$rows['admissionNumber']." class='form-control'></td>
+                                <td>" . $sn . "</td>
+                                <td>" . $rows['student_id'] . "</td>
+                                <td>" . $rows['first_name'] . "</td>
+                                <td>" . $rows['last_name'] . "</td>
+                                <td>" . $rows['course_id'] . "</td>
+                                <td>" . $rows['class_id'] . "</td>
+                                <td><input name='check[]' type='checkbox' value=" . $rows['student_id'] . " class='form-control'></td>
+                                <input name='admissionNo[]' value=" . $rows['student_id'] . " type='hidden' class='form-control'>
                               </tr>";
-                              echo "<input name='admissionNo[]' value=".$rows['admissionNumber']." type='hidden' class='form-control'>";
-                          }
-                      }
-                      else
-                      {
-                           echo   
-                           "<div class='alert alert-danger' role='alert'>
+                        }
+                      } else {
+                        echo
+                        "<div class='alert alert-danger' role='alert'>
                             No Record Found!
                             </div>";
                       }
-                      
+
                       ?>
                     </tbody>
                   </table>
                   <br>
-                  <button type="submit" name="save" class="btn btn-primary">Take Attendance</button>
-                  </form>
+                  <button type="submit" name="takeAttendance" class="btn btn-primary">Take Attendance</button>
+                  <?php
+                  if (isset($_POST['takeAttendance'])) {
+                    $check = isset($_POST['check']) ? $_POST['check'] : [];
+                    echo "Hunter";
+
+                    foreach ($check as $student_id) {
+                      // Determine the status based on whether the checkbox is ticked
+                      $attendanceStatus = isset($_POST['check'][$student_id]) ? 'Present' : 'Absent';
+
+                      // Retrieve class_id from the initial fetch loop
+                      $class_id = $rows['class_id'];
+                      $instructor_id = $_SESSION['instructor_id'];
+                      $date = date("Y-m-d"); // Current date
+
+                      // Insert or update the attendance record
+                      $insertQuery = "INSERT INTO attendance (class_id, instructor_id, student_id, `date`, `status`) 
+                        VALUES ('$class_id', '$instructor_id', '$student_id', '$date', '$attendanceStatus')
+                        ON DUPLICATE KEY UPDATE status = '$attendanceStatus'";
+
+                      $conn->query($insertQuery);
+                    }
+
+                    echo "<div class='alert alert-success' role='alert'>
+                            Attendance has been saved successfully!
+                          </div>";
+                  }
+                  ?>
                 </div>
               </div>
             </div>
-            </div>
           </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </body>
+
 </html>
