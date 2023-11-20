de7913f49d4de7a6ae209e8c9d9a976fe3da594a
Manpreet Singh <msp99000@gmail.com>
2023-11-21 01:24:50 +0530

    chore(fix): popup display for invalid credentials

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>DAMS - Login</title>
     <link href="assets/images/favicon.png" rel="icon">
     <link rel="stylesheet" href="css/login.css">
 </head>
+
 <body>
     <div class="login-container">
         <div class="login-card">
@@ -13,7 +15,7 @@
             <form method="POST" action="scripts/login.php">
                 <div class="input-group">
                     <select id="user-type" name="user-type" class="rounded-input" required>
-                        <option value=""> - -  Select User  - - </option>
+                        <option value=""> - - Select User - - </option>
                         <option value="Administrator">Administrator</option>
                         <option value="Instructor">Instructor</option>
                         <option value="Student">Student</option>
@@ -25,16 +27,17 @@
                 <div class="input-group">
                     <input type="password" id="password" name="password" class="rounded-input" placeholder="Password" required>
                 </div>
-			    <?php if(isset($_GET['error'])){ ?> <h4 style="color:red;"> Invalid username or password.</h1> <?php }?>
-                <div class="input-group">
-                    <input type="submit" class="rounded-button" name="login-button" value="Login">
-                </div>
-                <div class="forgot-password-link">
-                    <a href="forgot_password.php">Forgot Password?</a>
-                </div>
+                <?php if (isset($_GET['error'])) { ?> <h4 style="color:red;"> Invalid Username or Password.</h1> <?php } ?>
+                    <div class="input-group">
+                        <input type="submit" class="rounded-button" name="login-button" value="Login">
+                    </div>
+                    <div class="forgot-password-link">
+                        <a href="forgot_password.php">Forgot Password?</a>
+                    </div>
             </form>
         </div>
         <?php include "components/footer.php" ?>
     </div>
 </body>
+
 </html>
