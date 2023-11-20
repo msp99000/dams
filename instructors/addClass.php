124b4ba90d4cff1c351b0dd67e8930d93ea7c9e5
Manpreet Singh <msp99000@gmail.com>
2023-11-21 04:38:54 +0530

    chore(fix): fixes column mapping of classes table

--- instructors/addClass.php
+++ instructors/addClass.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-   <?php include "heads.php"; ?> 
+    <?php include "heads.php"; ?>
 </head>
+
 <body id="page-top">
     <?php include "topbar.php"; ?>
     <div id="content-wrapper" class="d-flex flex-column">
@@ -11,30 +13,50 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card mb-4">
-                            <div class="welcome-text">
-                                Add New Student
+                            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                <h1 class="h3 mb-0 text-gray-800"></h1>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="./">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Instructor Panel</li>
+                                </ol>
+                            </div>
+                            <div class="table-text">
+                                <img src="../assets/images/classes.png">
+                                <div>
+                                    Add Class
+                                </div>
                             </div>
                             <div class="card-body">
                                 <form method="post" action="process.php">
                                     <div class="form-group row mb-3">
                                         <div class="col-xl-6">
                                             <label class="form-control-label">Section<span class="text-danger ml-2">*</span></label>
-                                            <input type="text" class="form-control" name="firstName" id="exampleInputFirstName" placeholder="Section">
+                                            <input type="text" class="form-control" name="section" id="exampleInputFirstName" placeholder="Section">
                                         </div>
                                         <div class="col-xl-6">
                                             <label class="form-control-label">Course ID<span class="text-danger ml-2">*</span></label>
                                             <input type="text" class="form-control" name="courseId" id="exampleInputFirstName" placeholder="Course ID">
                                         </div>
                                     </div>
-                                    <button type="submit" name="addStudent" class="btn btn-primary">Add</button>
+                                    <div class="form-group row mb-3">
+                                        <div class="col-xl-6">
+                                            <label class="form-control-label">Session ID<span class="text-danger ml-2">*</span></label>
+                                            <input type="text" class="form-control" name="sessionId" id="exampleInputFirstName" placeholder="Session ID">
+                                        </div>
+                                        <div class="col-xl-6">
+                                            <!-- empty -->
+                                        </div>
+                                    </div>
+                                    <button type="submit" name="addClass" class="btn btn-primary">Add</button>
                                 </form>
                             </div>
                         </div>
-                        <?php include "classesTable.php"; ?>
+                        <?php include "../components/footer.php"; ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
