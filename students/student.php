7ec79c3a01af1f1eb3294529dd165bfd4506e1bb
Manpreet Singh <msp99000@gmail.com>
2023-11-21 02:32:12 +0530

    feat: adds schedule counter and leaves counter

--- students/student.php
+++ students/student.php
@@ -4,30 +4,33 @@ include "../config/connection.php";
 $database = new Database();
 $conn = $database->getConnection();
 
-class Student {
+class Student
+{
 
-    public function count_attendance(){
+    public function count_attendance()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM attendance"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
         } else {
             return 0; // Return 0 if an error occurs
         }
-    }  
+    }
 
-    public function count_students(){
+    public function count_students()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM students"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
@@ -36,13 +39,14 @@ class Student {
         }
     }
 
-    public function count_classes(){
+    public function count_classes()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM classes"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
@@ -51,13 +55,14 @@ class Student {
         }
     }
 
-    public function count_courses(){
+    public function count_courses()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM courses"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
@@ -66,13 +71,14 @@ class Student {
         }
     }
 
-    public function count_sessions(){
+    public function count_sessions()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM sessions"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
@@ -81,13 +87,14 @@ class Student {
         }
     }
 
-    public function count_terms(){
+    public function count_terms()
+    {
         global $conn;
-    
+
         $query = "SELECT COUNT(*) as total FROM terms"; // Use an alias for the count
-    
+
         $result = $conn->query($query);
-    
+
         if ($result) {
             $row = $result->fetch_assoc();
             return $row['total']; // Return the count
@@ -96,9 +103,42 @@ class Student {
         }
     }
 
-    public function list_students() {
+    public function count_leaves()
+    {
         global $conn;
-        
+
+        $query = "SELECT COUNT(*) as total FROM leaves"; // Use an alias for the count
+
+        $result = $conn->query($query);
+
+        if ($result) {
+            $row = $result->fetch_assoc();
+            return $row['total']; // Return the count
+        } else {
+            return 0; // Return 0 if an error occurs
+        }
+    }
+
+    public function count_schedules()
+    {
+        global $conn;
+
+        $query = "SELECT COUNT(*) as total FROM schedules"; // Use an alias for the count
+
+        $result = $conn->query($query);
+
+        if ($result) {
+            $row = $result->fetch_assoc();
+            return $row['total']; // Return the count
+        } else {
+            return 0; // Return 0 if an error occurs
+        }
+    }
+
+    public function list_students()
+    {
+        global $conn;
+
         $query = "SELECT * FROM students";
         $rs = $conn->query($query);
         $users = array(); // Create an array to store all user rows
@@ -110,4 +150,3 @@ class Student {
         return $users;
     }
 }
-?>
