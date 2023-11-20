d7ae42bc1f46cd0c35f42b319f34c6b4d46d432e
Manpreet Singh <msp99000@gmail.com>
2023-11-20 17:19:38 +0530

    chore(fix): fixes classes table columns

--- admin/classesTable.php
+++ admin/classesTable.php
@@ -1,12 +1,12 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="card mb-4">
-        <div class="table-text">
-            <img src="../assets/images/classes.png" >
-            <div> 
-                Classes
+            <div class="table-text">
+                <img src="../assets/images/classes.png">
+                <div>
+                    Classes
+                </div>
             </div>
-        </div>
             <div class="card-body">
                 <div class="table-responsive p-3">
                     <table class="table align-items-center table-flush table-hover" id="dataTableHover">
@@ -14,6 +14,8 @@
                             <tr>
                                 <th>Class ID</th>
                                 <th>Section</th>
+                                <th>Course ID</th>
+                                <th>Session ID</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -26,6 +28,8 @@
                                 echo "<tr>";
                                 echo "<td>" . $user['class_id'] . "</td>";
                                 echo "<td>" . $user['section'] . "</td>";
+                                echo "<td>" . $user['course_id'] . "</td>";
+                                echo "<td>" . $user['session_id'] . "</td>";
                                 echo "</tr>";
                             }
                             ?>
@@ -35,4 +39,4 @@
             </div>
         </div>
     </div>
-</div> 
+</div>
