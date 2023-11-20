0a69978401540afe15df03edf1c57d927253f5df
Manpreet Singh <msp99000@gmail.com>
2023-11-20 17:25:01 +0530

    feat: adds new feature to remove a student

--- admin/removeStudent.php
+++ admin/removeStudent.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <?php include "heads.php" ?>
 </head>
+
 <body id="page-top">
-    <?php include "topbar.php"; ?> 
+    <?php include "topbar.php"; ?>
     <div id="content-wrapper" class="d-flex flex-column">
         <div id="content">
             <div class="container-fluid" id="container-wrapper">
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="card mb-4">
-                            <div class="welcome-text">
-                                Remove Student
+                            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                <h1 class="h3 mb-0 text-gray-800"></h1>
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="./">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Admin Panel</li>
+                                </ol>
+                            </div>
+                            <div class="table-text">
+                                <img src="../assets/images/student.png">
+                                <div>
+                                    Remove Student
+                                </div>
                             </div>
                             <div class="card-body">
                                 <form method="post" action="process.php">
@@ -28,11 +40,12 @@
                                 </form>
                             </div>
                         </div>
-                       <?php include "studentsTable.php"; ?>
+                        <?php include "../components/footer.php"; ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </body>
+
 </html>
