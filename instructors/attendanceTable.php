da0ec1e01ff8429a1a5fd03e537a703534fbed8d
Manpreet Singh <msp99000@gmail.com>
2023-11-20 17:47:59 +0530

    chore(fix): fixes attendance table columns

--- instructors/attendanceTable.php
+++ instructors/attendanceTable.php
@@ -9,21 +9,22 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="card mb-4">
-            <div class="table-text">
-                <img src="../assets/images/attendance.png" >
-                <div> 
-                    Attendance
+                <div class="table-text">
+                    <img src="../assets/images/attendance.png">
+                    <div>
+                        Attendance
+                    </div>
                 </div>
-            </div>
                 <div class="card-body">
                     <div class="table-responsive p-3">
                         <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                             <thead class="thead-light">
                                 <tr>
                                     <th>Attendance ID</th>
-                                    <th>Course ID</th>
+                                    <th>Class ID</th>
                                     <th>Instructor ID</th>
                                     <th>Student ID</th>
+                                    <th>Course ID</th>
                                     <th>Date</th>
                                     <th>Status</th>
                                 </tr>
@@ -37,9 +38,10 @@
                                 foreach ($users as $user) {
                                     echo "<tr>";
                                     echo "<td>" . $user['attendance_id'] . "</td>";
-                                    echo "<td>" . $user['course_id'] . "</td>";
+                                    echo "<td>" . $user['class_id'] . "</td>";
                                     echo "<td>" . $user['instructor_id'] . "</td>";
                                     echo "<td>" . $user['student_id'] . "</td>";
+                                    echo "<td>" . $user['course_id'] . "</td>";
                                     echo "<td>" . $user['date'] . "</td>";
                                     echo "<td>" . $user['status'] . "</td>";
                                     echo "</tr>";
@@ -50,6 +52,7 @@
                     </div>
                 </div>
             </div>
+            <?php include "../components/footer.php"; ?>
         </div>
     </div>
 </div>
