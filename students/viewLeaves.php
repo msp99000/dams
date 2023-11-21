037acb43a24916b92511ac2edaec55e69433cac1
Manpreet Singh <msp99000@gmail.com>
2023-11-21 06:19:18 +0530

    feat: adds feature to view student leaves

---
+++ students/viewLeaves.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include "heads.php"; ?>
+</head>
+
+<body>
+    <?php include "topbar.php"; ?>
+    <?php include "leavesTable.php"; ?>
+</body>
+
+</html>
