b7d8f9911183a92b1f164028d5f54488e2575347
Manpreet Singh <msp99000@gmail.com>
2023-11-21 06:20:59 +0530

    chore(fix): fixes leave requests panel UI

--- students/leaveRequest.php
+++ students/leaveRequest.php
@@ -25,12 +25,61 @@
                         <h6 class="m-0 font-weight-bold text-primary">Submit Leave Request</h6>
                     </div>
                     <div class="card-body">
-                        <form method="post">
-                            <div class="form-group">
-                                <label for="reason">Reason for Leave:</label>
-                                <textarea class="form-control" id="reason" name="reason" rows="4" required></textarea>
+                        <form method="post" action="process.php">
+                            <div class="form-group row mb-3">
+                                <div class="col-xl-6">
+                                    <label for="reason">Reason for Leave:</label>
+                                    <textarea class="form-control" id="reason" name="reason" rows="4" required></textarea>
+                                </div>
+                                <div class="col-xl-6">
+                                    <!-- empty  -->
+                                </div>
                             </div>
-                            <button type="submit" name="submitLeave" class="btn btn-primary">Submit Leave Request</button>
+                            <div class="form-group row mb-3">
+                                <div class="col-xl-6">
+                                    <label class="form-control-label">Instructor ID<span class="text-danger ml-2">*</span></label>
+                                    <select class="form-control mb-3" id="class" name="instructorId" required>
+                                        <option value="" disabled selected>-- Select --</option>
+                                        <?php
+                                        // Include the class file
+                                        require_once('../options.php');
+
+                                        // Create an instance of the DatabaseHandler class
+                                        $users = new Options();
+
+                                        // Get class options
+                                        $classOptions = $users->instructorId_options();
+
+                                        // Populate select options
+                                        foreach ($classOptions as $option) {
+                                            echo "<option value='" . $option . "'>" . $option . "</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div>
+                                <!-- <div class="col-xl-6">
+                                    <label class="form-control-label">Class ID<span class="text-danger ml-2">*</span></label>
+                                    <select class="form-control mb-3" id="class" name="classId" required>
+                                        <option value="" disabled selected>-- Select --</option>
+                                        <?php
+                                        // Include the class file
+                                        require_once('../options.php');
+
+                                        // Create an instance of the DatabaseHandler class
+                                        $users = new Options();
+
+                                        // Get class options
+                                        $classOptions = $users->classId_options();
+
+                                        // Populate select options
+                                        foreach ($classOptions as $option) {
+                                            echo "<option value='" . $option . "'>" . $option . "</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                </div> -->
+                            </div>
+                            <button type="submit" name="addAdmin" class="btn btn-primary">Add</button>
                         </form>
                     </div>
                 </div>
@@ -42,10 +91,12 @@
     if (isset($_POST['submitLeave'])) {
         $studentId = $_SESSION['student_id'];
         $reason = $_POST['reason'];
+        $instructorId = $_POST['instructorId'];
+        $classId = $_POST['classId'];
 
         // Insert leave request into the database
-        $insertQuery = "INSERT INTO leaves (student_id, reason, request_date, `status`) 
-                    VALUES ('$studentId', '$reason', CURDATE(), 'Pending')";
+        $insertQuery = "INSERT INTO leaves (student_id, instructor_id, class_id, reason, request_date, `status`) 
+                    VALUES ('$studentId', '$instructorId', '$classId', '$reason', CURDATE(), 'Pending')";
 
         if ($conn->query($insertQuery) === TRUE) {
             echo "<div class='alert alert-success' role='alert'>
