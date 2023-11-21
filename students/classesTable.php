fba22e4e3675de8d4b5d41517b877b1bf8c13ca3
Manpreet Singh <msp99000@gmail.com>
2023-11-21 06:18:06 +0530

    feat: adds classes table

---
+++ students/classesTable.php
@@ -0,0 +1,42 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card mb-4">
+            <div class="table-text">
+                <img src="../assets/images/classes.png">
+                <div>
+                    Classes
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive p-3">
+                    <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                        <thead class="thead-light">
+                            <tr>
+                                <th>Class ID</th>
+                                <th>Section</th>
+                                <th>Course ID</th>
+                                <th>Session ID</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include "student.php";
+                            $admin = new Student();
+                            $users = $admin->view_classes();
+
+                            foreach ($users as $user) {
+                                echo "<tr>";
+                                echo "<td>" . $user['class_id'] . "</td>";
+                                echo "<td>" . $user['section'] . "</td>";
+                                echo "<td>" . $user['course_id'] . "</td>";
+                                echo "<td>" . $user['session_id'] . "</td>";
+                                echo "</tr>";
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
