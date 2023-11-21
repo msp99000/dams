4cbce3d8e6758f95a7802362d92a7c8b1ff2b579
Manpreet Singh <msp99000@gmail.com>
2023-11-21 06:31:02 +0530

    feat: adds courses table display

---
+++ students/coursesTable.php
@@ -0,0 +1,49 @@
+<div class="container-fluid" id="container-wrapper">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"></h1>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="./">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Student Panel</li>
+        </ol>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card mb-4">
+                <div class="table-text">
+                    <img src="../assets/images/courses.png">
+                    <div>
+                        Courses
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive p-3">
+                        <table class="table align-items-center table-flush table-hover" id="dataTableHover">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>Course ID</th>
+                                    <th>Course Code</th>
+                                    <th>Course Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                include "instructor.php";
+                                $admin = new Student();
+                                $users = $admin->my_courses();
+
+                                foreach ($users as $user) {
+                                    echo "<tr>";
+                                    echo "<td>" . $user['course_id'] . "</td>";
+                                    echo "<td>" . $user['course_code'] . "</td>";
+                                    echo "<td>" . $user['course_name'] . "</td>";
+                                    echo "</tr>";
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
