085a6cd4f14b2a2c6e554b64b60961ef93ce5d45
Manpreet Singh <msp99000@gmail.com>
2023-11-23 00:48:05 +0530

    feat: adds feature to help user regain the login

--- config/forgotPassword.php
+++ config/forgotPassword.php
@@ -1,94 +1,69 @@
-<?php 
-include 'Includes/dbcon.php';
-session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
+    <title>DAMS - Forgot Password</title>
+    <link href="assets/images/favicon.png" rel="icon">
+    <link rel="stylesheet" href="css/login.css">
+    <style>
+        .reset-card {
+            max-width: 400px;
+            margin: auto;
+        }
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="img/logo/attnlg.jpg" rel="icon">
-    <title>Code Camp BD - Forget Password</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/ruang-admin.min.css" rel="stylesheet">
+        .reset-card img {
+            margin: 0 auto 20px;
+            display: block;
+        }
 
+        .reset-card h2 {
+            margin-bottom: 20px;
+            color: #333;
+            font-size: 24px;
+        }
+
+        .reset-card .rounded-input {
+            margin-bottom: 15px;
+        }
+
+        .reset-card .rounded-button {
+            margin-top: 20px;
+        }
+
+        .reset-card .login-link {
+            margin-top: 15px;
+            font-size: 14px;
+        }
+    </style>
 </head>
 
-<body class="bg-gradient-login">
-    <!-- Login Content -->
-    <div class="container-login">
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card shadow-sm my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="login-form">
-                                    <div class="text-center">
-                                        <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
-                                        <br><br>
-                                        <h1 class="h4 text-gray-900 mb-4">Forgot Password</h1>
-                                    </div>
-                                    <form class="user" method="Post" action="">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control" required name="email" id="exampleInputEmail" placeholder="Enter Email Address">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <!-- <label class="custom-control-label" for="customCheck">Remember
-                          Me</label> -->
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-primary btn-block" value="Submit" name="submit" />
-                                        </div>
-                                    </form>
-
-                                    <?php
-
-              if(isset($_POST['submit'])){
-					
-                     
-				}
-			?>
-
-                                    <!-- <hr>
-                    <a href="index.html" class="btn btn-google btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a> -->
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="font-weight-bold small" href="memberSetup.php">Create a Memeber Account!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                        <a class="font-weight-bold small" href="organizationSetup.php">Setup Cooperative Account!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                        <a class="font-weight-bold small" href="forgotPassword.php">Forgot Password?</a>
-
-                                    </div>
-                                    <div class="text-center">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<body>
+    <div class="login-container">
+        <div class="reset-card">
+            <img src="assets/images/logo.png" alt="DAMS Logo" style="width:250px;height:250px">
+            <h2>Forgot Password</h2>
+            <form method="POST" action="forgot_password.php">
+                <div class="input-group">
+                    <select id="user-type" name="user-type" class="rounded-input" required>
+                        <option value=""> - - Select User - - </option>
+                        <option value="Administrator">Administrator</option>
+                        <option value="Instructor">Instructor</option>
+                        <option value="Student">Student</option>
+                    </select>
                 </div>
-            </div>
+                <div class="input-group">
+                    <input type="text" id="username" name="username" class="rounded-input" placeholder="Username" required>
+                </div>
+                <div class="input-group">
+                    <input type="submit" class="rounded-button" name="forgot-password-button" value="Reset Password">
+                </div>
+                <div class="login-link">
+                    <a href="index.php">Back to Login</a>
+                </div>
+            </form>
         </div>
+        <?php include "../components/footer.php"; ?>
     </div>
-    <!-- Login Content -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/ruang-admin.min.js"></script>
 </body>
 
 </html>
