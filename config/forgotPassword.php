634dcecf1b322941499911f6691a39ef463fb316
Manpreet Singh <msp99000@gmail.com>
2023-11-24 16:26:41 +0530

    style(UI): Classic card format DAMS UI matched for Forgot Password panle

--- config/forgotPassword.php
+++ config/forgotPassword.php
@@ -1,48 +1,43 @@
+<?php session_start();
+
+include "../config/connection.php";
+include "../scripts/authenticate.php";
+
+$database = new Database();
+$conn = $database->getConnection();
+
+$userAuth = new UserAuth($conn);
+
+if (isset($_POST['forgotpassword'])) {
+    $userType = $_POST['user-type'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+
+    $result = $userAuth->sendPasswordResetEmail($userType, $username, $email);
+
+    if ($result === true) {
+        $success_message = "pass";
+        header("Location: ../index.php?success=$success_message");
+    } else {
+        $error_message = "fail";
+        header("Location: forgotpassword.php?error=$error_message");
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>DAMS - Forgot Password</title>
-    <link href="assets/images/favicon.png" rel="icon">
-    <link rel="stylesheet" href="css/login.css">
-    <style>
-        .reset-card {
-            max-width: 400px;
-            margin: auto;
-        }
-
-        .reset-card img {
-            margin: 0 auto 20px;
-            display: block;
-        }
-
-        .reset-card h2 {
-            margin-bottom: 20px;
-            color: #333;
-            font-size: 24px;
-        }
-
-        .reset-card .rounded-input {
-            margin-bottom: 15px;
-        }
-
-        .reset-card .rounded-button {
-            margin-top: 20px;
-        }
-
-        .reset-card .login-link {
-            margin-top: 15px;
-            font-size: 14px;
-        }
-    </style>
+    <link href="../assets/images/favicon.png" rel="icon">
+    <link rel="stylesheet" href="../css/login.css">
 </head>
 
 <body>
     <div class="login-container">
-        <div class="reset-card">
-            <img src="assets/images/logo.png" alt="DAMS Logo" style="width:250px;height:250px">
-            <h2>Forgot Password</h2>
-            <form method="POST" action="forgot_password.php">
+        <div class="login-card">
+            <h1>Forgot Password</h1></br>
+            <form method="POST" action="">
                 <div class="input-group">
                     <select id="user-type" name="user-type" class="rounded-input" required>
                         <option value=""> - - Select User - - </option>
@@ -55,14 +50,18 @@
                     <input type="text" id="username" name="username" class="rounded-input" placeholder="Username" required>
                 </div>
                 <div class="input-group">
-                    <input type="submit" class="rounded-button" name="forgot-password-button" value="Reset Password">
+                    <input type="email" id="email" name="email" class="rounded-input" placeholder="Email" required>
                 </div>
-                <div class="login-link">
-                    <a href="index.php">Back to Login</a>
-                </div>
+                <?php if (isset($_GET['error'])) { ?> <h4 style="color:red;"> Invalid Username or Email.</h1> <?php } ?>
+                    <div class="input-group">
+                        <input type="submit" class="rounded-button" name="forgotpassword" value=" Send Password">
+                    </div>
+                    <div class="forgot-password-link">
+                        <a href="../index.php">Back to Login</a>
+                    </div>
             </form>
         </div>
-        <?php include "../components/footer.php"; ?>
+        <?php include "../components/footer.php" ?>
     </div>
 </body>
 
