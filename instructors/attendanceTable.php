ae08dbdc8999027dcbafd1d45090664770c7a4ae
Manpreet Singh <msp99000@gmail.com>
2023-11-26 03:14:06 +0530

    feat: introduces feature to download the attendance report

--- instructors/attendanceTable.php
+++ instructors/attendanceTable.php
@@ -1,6 +1,13 @@
+<?php
+session_start();
+?>
+
+<!-- Include TableExport library -->
+<script src="https://unpkg.com/tableexport.jquery.plugin/tableExport.min.js"></script>
+
 <div class="container-fluid" id="container-wrapper">
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800"></h1>
+        <h1 class="h3 mb-0 text-gray-800"><?php echo $_SESSION['firstName'] . ' ' . $_SESSION['lastName'] . ' (' . $_SESSION['instructor_id'] . ')'; ?></h1>
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="./">Home</a></li>
             <li class="breadcrumb-item active" aria-current="page">Instructor Panel</li>
@@ -12,7 +19,7 @@
                 <div class="table-text">
                     <img src="../assets/images/attendance.png">
                     <div>
-                        Attendance
+                        Class Attendance
                     </div>
                 </div>
                 <div class="card-body">
@@ -20,9 +27,9 @@
                         <table class="table align-items-center table-flush table-hover" id="dataTableHover">
                             <thead class="thead-light">
                                 <tr>
-                                    <th>Attendance ID</th>
+                                    <!-- <th>Attendance ID</th> -->
                                     <th>Class ID</th>
-                                    <th>Instructor ID</th>
+                                    <!-- <th>Instructor ID</th> -->
                                     <th>Student ID</th>
                                     <th>Course ID</th>
                                     <th>Date</th>
@@ -32,14 +39,15 @@
                             <tbody>
                                 <?php
                                 include "instructor.php";
-                                $admin = new Instructor();
-                                $users = $admin->list_attendance();
+                                $id = $_SESSION['instructor_id'];
+                                $instructor = new Instructor();
+                                $users = $instructor->list_attendance($id);
 
                                 foreach ($users as $user) {
                                     echo "<tr>";
-                                    echo "<td>" . $user['attendance_id'] . "</td>";
+                                    // echo "<td>" . $user['attendance_id'] . "</td>";
                                     echo "<td>" . $user['class_id'] . "</td>";
-                                    echo "<td>" . $user['instructor_id'] . "</td>";
+                                    // echo "<td>" . $user['instructor_id'] . "</td>";
                                     echo "<td>" . $user['student_id'] . "</td>";
                                     echo "<td>" . $user['course_id'] . "</td>";
                                     echo "<td>" . $user['date'] . "</td>";
@@ -49,6 +57,7 @@
                                 ?>
                             </tbody>
                         </table>
+                        <button class=rounded-success-button id="exportButton">Export to Excel</button>
                     </div>
                 </div>
             </div>
