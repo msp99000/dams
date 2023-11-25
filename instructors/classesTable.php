fc72fa85d9c7492471a955354a69706aa0e5c38b
Manpreet Singh <msp99000@gmail.com>
2023-11-26 03:14:49 +0530

    refactor: adds session start to top

--- instructors/classesTable.php
+++ instructors/classesTable.php
@@ -1,10 +1,15 @@
+<?php
+
+session_start();
+
+?>
 <div class="row">
     <div class="col-lg-12">
         <div class="card mb-4">
             <div class="table-text">
                 <img src="../assets/images/classes.png">
                 <div>
-                    Classes
+                    My Classes
                 </div>
             </div>
             <div class="card-body">
@@ -14,22 +19,27 @@
                             <tr>
                                 <th>Class ID</th>
                                 <th>Section</th>
-                                <th>Course ID</th>
-                                <th>Session ID</th>
+                                <th>Schedule</th>
+                                <th>Status</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php
+
                             include "instructor.php";
-                            $admin = new Instructor();
-                            $users = $admin->list_classes();
+
+                            $id = $_SESSION['instructor_id'];
+
+                            $instructor = new Instructor();
+
+                            $users = $instructor->list_classes($id);
 
                             foreach ($users as $user) {
                                 echo "<tr>";
                                 echo "<td>" . $user['class_id'] . "</td>";
                                 echo "<td>" . $user['section'] . "</td>";
-                                echo "<td>" . $user['course_id'] . "</td>";
-                                echo "<td>" . $user['session_id'] . "</td>";
+                                echo "<td>" . $user['schedule'] . "</td>";
+                                echo "<td>" . $user['status'] . "</td>";
                                 echo "</tr>";
                             }
                             ?>
